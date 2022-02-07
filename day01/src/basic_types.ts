f0b477645fbdd1f1134519143756a83b70f5bde2
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2022-02-07 15:18:41 +0530

    Day01

---
+++ day01/src/basic_types.ts
@@ -0,0 +1,22 @@
+//number, boolean, string, or array or any other type
+//variableName: type
+//default value is undefined
+
+let age: number = 12;
+let comment: string = 'Sleepy';
+let comment2: string = "Sleepy";
+let isCool: boolean = false;
+
+//Type inference
+let salary = 1000;
+//salary = "3000";
+
+let i;
+if (i !== undefined) {
+    console.log(i);
+} else {
+    console.log('i is undefined');
+}
+let isHot = true;
+console.log(typeof(isHot));
+
