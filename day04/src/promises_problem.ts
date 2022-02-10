ae3b7171d786b7bcfb8ff946faea8866a8a8086b
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2022-02-10 15:33:00 +0530

    Day04

---
+++ day04/src/promises_problem.ts
@@ -0,0 +1,22 @@
+//generate a random number
+//increments by 1
+//square the number
+//print the result on the console
+
+const generateRandom = () => Math.round(Math.random() * 100);
+const increment = (num: number) => num + 1;
+const square = (num: number) => num * num;
+const printIt = (result: number) => console.log("Result", result);
+
+function doSomething() {
+    try {
+        let num = generateRandom();
+        let incValue = increment(num);
+        let squaredValue = square(incValue);
+        printIt(squaredValue);
+    } catch (error) {
+        console.log(error);
+    }
+}
+doSomething();
+console.log("end of program");
